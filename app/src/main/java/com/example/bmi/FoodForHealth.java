7b5fefe613b87c2396aa799a96346a7734ab57ce
honnage <hon_da-1998@hotmail.com>
2020-09-08 17:26:21 +0700

    BMI Application

---
+++ app/src/main/java/com/example/bmi/FoodForHealth.java
@@ -0,0 +1,14 @@
+package com.example.bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodForHealth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_for_health);
+    }
+}
