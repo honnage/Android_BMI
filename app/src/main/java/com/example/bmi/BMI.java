401d2fa24806474f825650a4b91bd6f3529d91cd
honnage <hon_da-1998@hotmail.com>
2020-09-23 18:56:23 +0700

    Update latest

--- app/src/main/java/com/example/bmi/BMI.java
+++ app/src/main/java/com/example/bmi/BMI.java
@@ -12,15 +12,23 @@ public class BMI extends AppCompatActivity {
     String msgH;
     String BMI;
     String Lavel1 = "Weight less than standard";
+    String Lavel1TH = "น้ำหนักต่ำกว่าเกณฑ์";
     String Lavel2 = "Normal weight";
+    String Lavel2TH = "สมส่วน";
     String Lavel3 = "Obesity Level 1";
+    String Lavel3TH = "น้ำหนักเกิน";
     String Lavel4 = "Obesity Level 2";
+    String Lavel4TH = "โรคอ้วน";
     String Lavel5 = "Obesity Level 3";
+    String Lavel5TH = "โรคอ้วนอันตราย";
+
 
     TextView TvMsgWeightShow;
     TextView TvMsgHeightShow;
     TextView TvBMI;
     TextView TvLavelBMI;
+    TextView TvLavelBMI_Dedail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,7 @@ public class BMI extends AppCompatActivity {
         TvMsgHeightShow = findViewById(R.id.idEtHeightShow);
         TvBMI = findViewById(R.id.idBMI);
         TvLavelBMI = findViewById(R.id.idStrLavelBMI);
+        TvLavelBMI_Dedail = findViewById(R.id.idStrLavelBMI_TH);
 
         Intent i = getIntent();
         msgW = i.getStringExtra("MSG_W");
@@ -52,22 +61,32 @@ public class BMI extends AppCompatActivity {
         if(ValueBMI < 18.5){
             TvLavelBMI.setText(Lavel1);
             TvLavelBMI.setTextColor(Color.rgb(0,153,153)); //สีฟ้า
+            TvLavelBMI_Dedail.setText(Lavel1TH);
+            TvLavelBMI_Dedail.setTextColor(Color.rgb(0,153,153)); //สีฟ้า
 
         }else if(ValueBMI >= 18.5 && ValueBMI < 23){
             TvLavelBMI.setText(Lavel2);
             TvLavelBMI.setTextColor(Color.rgb(0,204,0)); //สีเขียว
+            TvLavelBMI_Dedail.setText(Lavel2TH);
+            TvLavelBMI_Dedail.setTextColor(Color.rgb(0,204,0)); //สีเขียว
 
         }else if(ValueBMI >= 23 && ValueBMI < 25){
             TvLavelBMI.setText(Lavel3);
             TvLavelBMI.setTextColor(Color.rgb(247,206,0)); //สีเหลือง
+            TvLavelBMI_Dedail.setText(Lavel3TH);
+            TvLavelBMI_Dedail.setTextColor(Color.rgb(247,206,0)); //สีเหลือง
 
         }else if(ValueBMI >= 25 && ValueBMI < 30) {
             TvLavelBMI.setText(Lavel4);
             TvLavelBMI.setTextColor(Color.rgb(255,128,0)); //สีส้ม
+            TvLavelBMI_Dedail.setText(Lavel4TH);
+            TvLavelBMI_Dedail.setTextColor(Color.rgb(255,128,0)); //สีส้ม
 
         }else{
             TvLavelBMI.setText(Lavel5);
             TvLavelBMI.setTextColor(Color.rgb(200,0,0)); //สีแดดง
+            TvLavelBMI_Dedail.setText(Lavel5TH);
+            TvLavelBMI_Dedail.setTextColor(Color.rgb(200,0,0)); //สีแดดง
 
         }
 
--- app/src/main/java/com/example/bmi/MainActivity.java
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -35,4 +35,19 @@ public class MainActivity extends AppCompatActivity {
         else
             Log.d("ImpliotImtents","cannit handle this intent.");
     }
+
+    public void WikiBMI (View v){
+        Uri web = Uri.parse("https://th.wikipedia.org/wiki/%E0%B8%94%E0%B8%B1%E0%B8%8A%E0%B8%99%E0%B8%B5%E0%B8%A1%E0%B8%A7%E0%B8%A5%E0%B8%81%E0%B8%B2%E0%B8%A2");
+        Intent i = new Intent(Intent.ACTION_VIEW,web);
+        //if(i.resolveActivity(getPackageManager()) != null )
+            startActivity(i);
+        /*else
+            Log.d("ImpliotImtents","cannit handle this intent.");*/
+    }
+
+    public void Article (View v){
+        Uri web = Uri.parse("https://bnnpost.com/2016/681");
+        Intent i = new Intent(Intent.ACTION_VIEW,web);
+        startActivity(i);
+    }
 }
